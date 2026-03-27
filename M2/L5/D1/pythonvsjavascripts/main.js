9ebd7d96286d40f65ef74beaff08e3048b7b91aa
blandskron <bastianlandskronfreelancer@gmail.com>
2026-03-26 21:50:06 -0300

    actualizando

---
+++ M2/L5/D1/pythonvsjavascripts/main.js
@@ -0,0 +1,13 @@
+// Contador del 1 al 5 con condición
+
+let numero = 1;
+
+while (numero <= 5) {
+    if (numero % 2 === 0) {
+        console.log(numero + " es par");
+    } else {
+        console.log(numero + " es impar");
+    }
+
+    numero++;
+}
---
+++ M2/L5/D1/pythonvsjavascripts/main.py
@@ -0,0 +1,11 @@
+# Contador del 1 al 5 con condición
+
+numero = 1
+
+while numero <= 5:
+    if numero % 2 == 0:
+        print(f"{numero} es par")
+    else:
+        print(f"{numero} es impar")
+    
+    numero += 1
