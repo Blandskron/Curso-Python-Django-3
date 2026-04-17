0b85a35c972b8e7b33e6eace2368f269e8d5bdc1
blandskron <bastianlandskronfreelancer@gmail.com>
2026-04-16 22:16:54 -0400

    update

--- M3/L5/D1/06_python.py
+++ M3/L5/D1/06_python.py
@@ -1,11 +1,17 @@
 # 6) List comprehension: transformar + filtrar
 numeros = list(range(10))
+
 cuadrados_pares = [n**2 for n in numeros if n % 2 == 0]
-print(cuadrados_pares)
 
 numeros2 = [] 
-
 for n in numeros:
     if n % 2 == 0:
         numeros2.append(n ** 2)
-print(numeros2)
+
+print(f"List comprehension: {cuadrados_pares}")
+print(f"List append method: {numeros2}")
+
+"""
+List comprehension: [0, 4, 16, 36, 64]
+List append method: [0, 4, 16, 36, 64]
+"""
